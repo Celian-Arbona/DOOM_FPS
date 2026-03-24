ad7cf3fff79a41836eb671de8dfa77a699003a77
adril <adril@MSI>
2026-03-24 14:56:56 +0100

    1step

---
+++ src/main/java/com/jad/fps/Map.java
@@ -0,0 +1,21 @@
+package com.jad.fps;
+
+public class Map {
+
+    private final int[][] maze;
+    private final int width;
+    private final int height;
+
+    public Map(int width, int height) {
+
+        this.width = width;
+        this.height = height;
+        this.maze = new int[height][width];
+        for (int row = 0; row < this.height; row++) {
+            for (int column = 0; column < this.width; column++) {
+                this.maze[row][column] = 0;
+            }
+        }
+    }
+
+}
---
+++ src/main/java/com/jad/fps/Renderer.java
@@ -0,0 +1,4 @@
+package com.jad.fps;
+
+public class Renderer {
+}
